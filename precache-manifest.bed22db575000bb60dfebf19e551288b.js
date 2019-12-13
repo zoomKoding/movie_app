2dd702bc15252a593282e11dd2eb1de80c98d235
zoomkoding <gur5381@gmail.com>
2019-12-13 23:53:12 +0900

    Updates

---
+++ precache-manifest.bed22db575000bb60dfebf19e551288b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c07e037a3fd10d79fa19956725610db",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "4683e5c9533555a35249",
+    "url": "/movie_app/static/css/main.81242767.chunk.css"
+  },
+  {
+    "revision": "6f0599dff9eecd2a0e73",
+    "url": "/movie_app/static/js/2.43e6c5f9.chunk.js"
+  },
+  {
+    "revision": "4683e5c9533555a35249",
+    "url": "/movie_app/static/js/main.3d7ea3a9.chunk.js"
+  },
+  {
+    "revision": "7259bafd2195d7f70cbd",
+    "url": "/movie_app/static/js/runtime-main.c454ae6d.js"
+  }
+]);
